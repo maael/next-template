68d9e5e30ea28cda1112927d2449b66765bc03c0
Matthew Elphick <matt.a.elphy@gmail.com>
2020-10-25 14:02:05 +0000

    chore: Tidy up template, fix eslint config

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,6 +1,6 @@
-const title = "Matt Elphick | Software Engineer";
-const description = "Software engineer mainly messing around with JavaScript.";
-const url = "https://mael.tech/";
+const title = 'Matt Elphick | Software Engineer'
+const description = 'Software engineer mainly messing around with JavaScript.'
+const url = 'https://mael.tech/'
 
 module.exports = {
   title,
@@ -11,12 +11,12 @@ module.exports = {
     description,
     url,
     site_name: title,
-    type: "website",
-    locale: "en_GB",
+    type: 'website',
+    locale: 'en_GB',
   },
   twitter: {
-    handle: "@mattaelphick",
-    site: "@mattaelphick",
-    cardType: "summary_large_image",
+    handle: '@mattaelphick',
+    site: '@mattaelphick',
+    cardType: 'summary_large_image',
   },
-};
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,3 @@
 export default function Index() {
-  return <div>Page</div>;
+  return <div>Page</div>
 }
