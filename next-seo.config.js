f916bf10730d8d812ee4a0153239e307eaecd916
Matthew Elphick <matt.a.elphy@gmail.com>
2022-10-30 20:44:35 +0000

    fix: Remove cananonical url
    This broke fathom url/path tracking

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,11 +1,9 @@
 const title = 'Matt Elphick | Software Engineer'
 const description = 'Software engineer mainly messing around with JavaScript.'
-const url = 'https://mael.tech/'
 
 export default {
   title,
   description,
-  canonical: url,
   openGraph: {
     title,
     description,
